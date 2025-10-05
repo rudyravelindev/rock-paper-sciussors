af8cc4b53674fe08bad785a58747113cbd286fe6
rudyravelindev <rudyravelindev@gmail.com>
2025-10-05 01:27:18 -0700

    creating the computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Test 123');
+function getComputerChoice(choice) {
+  let computerChoice = Math.floor(Math.random() * choice);
+
+  if (computerChoice == 0) {
+    return 'rock';
+  } else if (computerChoice == 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+console.log(getComputerChoice(3));

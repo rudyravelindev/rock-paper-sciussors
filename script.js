0c3184feafd4ca7a137fd87f3f65989e3bda38c6
rudyravelindev <rudyravelindev@gmail.com>
2025-10-09 21:34:49 -0700

    Done with this project

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Initial scores
+let humanScore = 0;
+let computerScore = 0;
+
 // Computer choice
 function getComputerChoice(choice) {
   let computerChoice = Math.floor(Math.random() * choice);
@@ -11,12 +15,54 @@ function getComputerChoice(choice) {
   }
 }
 
-console.log(getComputerChoice(3));
-
 // Human choice
 function getHumanChoice() {
   let humanChoice = prompt("Please enter 'rock', 'paper', 'scissors': ");
-  return humanChoice;
+  return humanChoice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+// PlayRound
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log('It is a Tie!');
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper') ||
+    (humanChoice === 'paper' && computerChoice === 'rock')
+  ) {
+    console.log(`Human WIN: ${humanChoice} beat ${computerChoice}`);
+    ++humanScore;
+  } else {
+    console.log(`Computer WIN ${computerChoice} beat ${humanChoice}`);
+    ++computerScore;
+  }
+}
+
+// Play Game
+
+function playGame() {
+  let roundsLeft = 5;
+
+  while (roundsLeft > 0) {
+    let computerChoice = getComputerChoice(3);
+    let humanChoice = getHumanChoice();
+    console.log(
+      `Human choose ${humanChoice} & Computer choose ${computerChoice}`
+    );
+    playRound(humanChoice, computerChoice);
+    console.log(`Human: ${humanScore} and Computer: ${computerScore}`);
+
+    roundsLeft--;
+  }
+
+  console.log('Game over!');
+  if (humanScore == computerScore) {
+    console.log('It is a tie');
+  } else if (humanScore > computerScore) {
+    console.log('Human Win the five round game');
+  } else {
+    console.log('Computer win the five round game');
+  }
+}
+
+playGame();
